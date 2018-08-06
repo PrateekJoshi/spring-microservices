367fd1608d9b00ce1338b577499e4a689789a3e7
Prateek Joshi <prateek.joshi@gemalto.com>
2018-08-06 20:52:29 +0530

    added currency conversion service

---
+++ currency-conversion-service/src/main/java/com/prateek/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,32 @@
+package com.prateek.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController	
+public class CurrencyConversionController {
+	
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency(@PathVariable String from,@PathVariable String to,@PathVariable BigDecimal quantity) {
+		/* Use Map to store URI variables from the request URI */
+		Map<String,String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		
+		/* Call the currency-exchange service to get currency exchange rates from the database and make a response entity */
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate()
+				.getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,uriVariables);
+		
+		/* From response entity, store the response in the bean */
+		CurrencyConversionBean response = responseEntity.getBody();
+		 
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+	}
+}
--- currency-exchange-service/src/main/java/com/prateek/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/prateek/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,31 @@
 package com.prateek.microservices.currencyexchangeservice;
 
-public class CurrencyExchangeController {
+import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;	//will be used to fetch values from property file
+	
+	@Autowired
+	private ExchangeValueRepository repository;
+	
+	/*
+	 * Get exchange rate from currency {from} to currency {to}
+	 */
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		/* Fetch values of exchange rate from database using JPA */
+		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+		/* Since we will run multiple instances of this service, fetch the running port of the instance dynamically */
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		return exchangeValue;
+	}
 }
