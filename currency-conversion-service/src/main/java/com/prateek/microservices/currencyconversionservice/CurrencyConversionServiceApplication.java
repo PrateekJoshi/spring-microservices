1d09a678add81f956cb7cecc35facf4676f867a1
Prateek Joshi <prateek.joshi@gemalto.com>
2018-08-07 11:51:43 +0530

    added load balancing using eureka naming server for currency-exchange-service

--- currency-conversion-service/src/main/java/com/prateek/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/prateek/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.prateek.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients		//enable feign client
+@EnableDiscoveryClient	//enable Eureka naming server discovery
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
